87c1a31ed26c9dc07c40ac9995986e0e10baba44
KoryakinaSvetlana <switch_sv@mail.ru>
2018-02-18 16:59:41 +0500

    Правки по критериям

--- gulpfile.js
+++ gulpfile.js
@@ -21,42 +21,42 @@ var run = require('run-sequence');
 /*Сбор и минимизация стилей*/
 gulp.task("style", function () {
   gulp.src("source/less/style.less")
-  .pipe(plumber())
-  .pipe(less())
-  .pipe(postcss([
-    autoprefixer()
-  ]))
-  .pipe(gulp.dest("build/css"))
-  .pipe(minifycss())
-  .pipe(rename("style.min.css"))
-  .pipe(gulp.dest("build/css"));
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minifycss())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"));
 });
 
 /*Минимизация js*/
 gulp.task('js', function () {
-    gulp.src(['source/js/*.js', '!source/js/*.min.js'])
-        .pipe(plumber())
-        .pipe(minifyjs())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('build/js'));
+  gulp.src(['source/js/*.js', '!source/js/*.min.js'])
+    .pipe(plumber())
+    .pipe(minifyjs())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('build/js'));
 });
 
 /*Сжатие изображений*/
 gulp.task("images", function () {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
-             .pipe(imagemin([
-               imagemin.optipng({optimizationLevel: 3}),
-               imagemin.jpegtran({progressive: true}),
-               imagemin.svgo()
-             ]))
-             .pipe(gulp.dest("source/img"));
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
 });
 
 /*WebP*/
 gulp.task("webp", function () {
   return gulp.src("source/img/**/*.{png,jpg}")
-             .pipe(webp({quality: 90}))
-             .pipe(gulp.dest("source/img"));
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"));
 });
 
 /*SVG Sprite*/
@@ -87,12 +87,12 @@ gulp.task("html", function () {
 /*Copy*/
 gulp.task("copy", function () {
   return gulp.src([
-            "source/fonts/**/*.{woff,woff2}",
-            "source/img/**",
-            "source/js/**"
-             ], {
-             base: "source"
-             })
+      "source/fonts/**/*.{woff,woff2}",
+      "source/img/**",
+      "source/js/**"
+      ], {
+      base: "source"
+      })
     .pipe(gulp.dest("build"));
 });
 
